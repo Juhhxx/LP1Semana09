46bc39152564fc5194423d93916b4f5a1431c285
Julia Costa <juliacosta232005@gmail.com>
2024-05-09 07:08:23 +0100

    Add loop and file write

--- FilePower1/Program.cs
+++ FilePower1/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace FilePower1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] fileName)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("START");
+            Queue<string> wordsTyped = new Queue<string>();
+            string userInput = null;
+            string path = Path.Combine("FilePower1",fileName[0]);
+
+            do
+            {
+                if (userInput != null)
+                    wordsTyped.Enqueue(userInput);
+                userInput = Console.ReadLine();
+            }while (userInput != ""); 
+
+            int size = wordsTyped.Count;
+            string fileText = "";
+
+            for (int i = 0; i < size; i++)
+            {
+                fileText += wordsTyped.Dequeue();
+                if (i != size-1)
+                    fileText += "\n";
+            }
+            File.WriteAllText(path,fileText);
         }
     }
 }
