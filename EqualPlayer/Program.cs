0e2c3de4e9cdd390a1c8d12681a7ba231090e33d
Julia Costa <juliacosta232005@gmail.com>
2024-05-09 05:30:05 +0100

    Add test for setofPlayers

--- EqualPlayer/Program.cs
+++ EqualPlayer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EqualPlayer
 {
@@ -6,7 +7,14 @@ namespace EqualPlayer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HashSet<Player> setOfPlayers = new HashSet<Player>();
+
+            setOfPlayers.Add(new Player(PlayerClass.Tank,"Ana"));
+            setOfPlayers.Add(new Player(PlayerClass.Slayer,"Paulo"));
+            setOfPlayers.Add(new Player(PlayerClass.Tank,"Ana"));
+
+            foreach (Player p in setOfPlayers)
+                Console.WriteLine($"{p.Name} is a {p.PClass}");
         }
     }
 }
