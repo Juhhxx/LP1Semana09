f6880e6d2e6007ab85e795c348a98ff985dd2368
Julia Costa <juliacosta232005@gmail.com>
2024-05-09 05:15:14 +0100

    Add class Player and Enum PlayerClass

---
+++ EqualPlayer/Player.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace EqualPlayer
+{
+    public class Player
+    {
+        public PlayerClass PClass { get; }
+        public string Name { get; }
+        public Player(PlayerClass pClass, string name)
+        {
+            PClass = pClass;
+            Name = name;
+        }
+
+        
+    }
+}
