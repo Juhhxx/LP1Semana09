1fcd3ef357121543833092dd8c4c2a8a337328e1
Julia Costa <juliacosta232005@gmail.com>
2024-05-10 15:22:28 +0100

    Add data storage method

--- CharSets/Program.cs
+++ CharSets/Program.cs
@@ -15,6 +15,30 @@ namespace CharSets
                 filesContent[i] = new HashSet<char>();
             }
 
+            for (int i = 0; i < fileNames.Length; i++)
+            {
+                string path = Path.Combine("CharSets",fileNames[i]);
+                using StreamReader sr = new StreamReader(path);
+
+                string s;
+
+                while ((s = sr.ReadLine()) != null)
+                {
+                    if (s.Length == 1)
+                    {
+                        char charS = char.Parse(s);
+                        filesContent[i].Add(charS);
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERRO");
+                        return;
+                    }
+
+                }
+            }
+            
+           
         }
     }
 }
